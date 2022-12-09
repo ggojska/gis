5097f6d475543eb384e558bbc42876e98f0ae822
MP <paczesnymaciej@gmail.com>
2022-12-09 16:04:16 +0100

    add lat, lon, radius params to gas stations api get req

--- app/api/gas_stations.py
+++ app/api/gas_stations.py
@@ -1,7 +1,5 @@
 from flask import jsonify, request, url_for, current_app
 
-
-from .. import db
 from ..models import GasStation
 from . import api
 
@@ -9,16 +7,29 @@ from . import api
 @api.route('/gas_stations/')
 def get_gas_stations():
     page = request.args.get('page', 1, type=int)
-    pagination = GasStation.query.paginate(
-        page=page, per_page=current_app.config['STATIONS_PER_PAGE'],
-        error_out=False)
+    lat = request.args.get('lat', type=float)
+    lon = request.args.get('lon', type=float)
+    radius = request.args.get('radius', type=int)
+
+    if lat and lon and radius:
+        # query = GasStation.query.filter(func.globe_distance(GasStation.lat, GasStation.lat, lat, lon) < radius)
+        query = GasStation.query
+    else:
+        query = GasStation.query
+
+    pagination = query.paginate(
+            page=page, per_page=current_app.config['STATIONS_PER_PAGE'],
+            error_out=False)
     stations = pagination.items
+
     prev = None
     if pagination.has_prev:
         prev = url_for('api.get_gas_stations', page=page-1)
+
     next = None
     if pagination.has_next:
         next = url_for('api.get_gas_stations', page=page+1)
+
     return jsonify({
         'gas_stations': [station.to_json() for station in stations],
         'prev': prev,
