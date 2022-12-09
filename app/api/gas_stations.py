55a279567de5a63ef0d8f792f7e74edaf15a258b
MP <paczesnymaciej@gmail.com>
2022-12-10 00:46:06 +0100

    use custom SQL query when selecting gas stations within radius

--- app/api/gas_stations.py
+++ app/api/gas_stations.py
@@ -1,6 +1,8 @@
 from flask import jsonify, request, url_for, current_app
+from sqlalchemy.sql import text
 
-from ..models import GasStation
+from ..models import GasStation, db
+from ..sql import sql
 from . import api
 
 
@@ -10,31 +12,32 @@ def get_gas_stations():
     lat = request.args.get('lat', type=float)
     lon = request.args.get('lon', type=float)
     radius = request.args.get('radius', type=int)
+    prev, next = None, None
 
     if lat and lon and radius:
-        # query = GasStation.query.filter(func.globe_distance(GasStation.lat, GasStation.lat, lat, lon) < radius)
-        query = GasStation.query
+        query = GasStation.query.from_statement(text(sql.select_gas_stations_with_distance)).\
+            params(lat=lat, lon=lon, radius=radius).all()
+        stations = query
+        total = len(stations)
+
     else:
         query = GasStation.query
-
-    pagination = query.paginate(
-            page=page, per_page=current_app.config['STATIONS_PER_PAGE'],
-            error_out=False)
-    stations = pagination.items
-
-    prev = None
-    if pagination.has_prev:
-        prev = url_for('api.get_gas_stations', page=page-1)
-
-    next = None
-    if pagination.has_next:
-        next = url_for('api.get_gas_stations', page=page+1)
+        pagination = query.paginate(
+                page=page, per_page=current_app.config['STATIONS_PER_PAGE'],
+                error_out=False)
+        stations = pagination.items
+        
+        if pagination.has_prev:
+            prev = url_for('api.get_gas_stations', page=page-1)
+        if pagination.has_next:
+            next = url_for('api.get_gas_stations', page=page+1)
+        total = pagination.total
 
     return jsonify({
         'gas_stations': [station.to_json() for station in stations],
         'prev': prev,
         'next': next,
-        'count': pagination.total
+        'count': total
     })
 
 @api.route('/gas_stations/<int:id>')
