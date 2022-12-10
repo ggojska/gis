c462669b5ddd90414e4b26ed2c2bf23593bd1813
MP <paczesnymaciej@gmail.com>
2022-12-10 21:14:08 +0100

    add order by, change celculated column name

--- app/sql/sql.py
+++ app/sql/sql.py
@@ -1,28 +1,31 @@
-select_gas_stations_with_distance = """
-SELECT 
+select_gas_stations_with_distance = \
+"""
+SELECT
     gas_stations.id AS gas_stations_id
     , gas_stations.name AS gas_stations_name
     , gas_stations.lat AS gas_stations_lat
-    , gas_stations.lon AS gas_stations_lon 
+    , gas_stations.lon AS gas_stations_lon
     , 2.0 * 6371.0 *
     atan2(
         sqrt(
-            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) * 
+            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) *
             sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) +
             cos(:lat * pi()/180.0) * cos(lat * pi()/180.0) *
-            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2) * 
+            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2) *
             sin((lon * pi()/180.0 - :lon * pi()/180.0)/2)
-        ), 
+        ),
         sqrt(1-
-            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) * 
+            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) *
             sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) +
             cos(:lat * pi()/180.0) * cos(lat * pi()/180.0) *
-            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2) * 
+            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2) *
             sin((lon * pi()/180.0 - :lon * pi()/180.0)/2)
         )
-        ) AS distance
+    ) AS gas_stations_distance
 FROM
     gas_stations
 WHERE
-    distance < :radius
+    gas_stations_distance < :radius
+ORDER BY
+    gas_stations_distance
 """
