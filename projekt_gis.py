874cc58bfeec7e7250ab98ccb289ebe453ad1faa
MPatchesny <strielok11+github@gmail.com>
2022-12-15 14:21:47 +0100

    fix importing sample data

--- projekt_gis.py
+++ projekt_gis.py
@@ -8,11 +8,14 @@ from app import models
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
-if os.getenv('IMPORT_SAMPLE_DATA') == "1":
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(basedir, "sample_data", "sample_data.sql")
-    if os.path.exists(path):
-        with open(path, "r", encoding="utf-8") as file:
-            data = file.read()
-            db.session.execute(data)
-            db.session.commit()
+with app.app_context():
+    if os.getenv('IMPORT_SAMPLE_DATA') == "1":
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(basedir, "sample_data", "sample_data.sql")
+        if os.path.exists(path):
+            print("Importing sample data...")
+            with open(path, "r", encoding="utf-8") as file:
+                data = file.readlines()
+                for line in data:
+                    db.session.execute(line)
+                db.session.commit()
