dfa722634219ba1ddfc6b63a510c24384d403248
M. Paczesny <strielok11+github@gmail.com>
2022-12-15 10:09:17 +0000

    fix indentation

--- projekt_gis.py
+++ projekt_gis.py
@@ -9,9 +9,9 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 if os.getenv('IMPORT_SAMPLE_DATA') == "1":
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(basedir, "sample_data", "sample_data.sql")
-        if os.path.exists(path):
-            with open(path, "r", encoding="utf-8") as file:
-                data = file.read()
-            db.session.execute(data)
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(basedir, "sample_data", "sample_data.sql")
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as file:
+            data = file.read()
+        db.session.execute(data)
