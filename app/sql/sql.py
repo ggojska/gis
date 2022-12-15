29628dbecf023daa2f111a3f4317eac2b007f864
MPatchesny <strielok11+github@gmail.com>
2022-12-15 14:21:40 +0100

    simplify distance calculation; add WHERE clause

--- app/sql/sql.py
+++ app/sql/sql.py
@@ -1,27 +1,26 @@
 select_gas_stations_with_distance = \
 """
 SELECT
-    gas_stations.id AS id
-    , gas_stations.name AS name
-    , gas_stations.lat AS lat
-    , gas_stations.lon AS lon
-    , ROUND(2.0 * 6371.0 * 1000 * 
-    atan2(
-        sqrt(
-            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) *
-            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) +
-            cos(:lat * pi()/180.0) * cos(lat * pi()/180.0) *
-            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2) *
-            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2)
-        ),
-        sqrt(1-
-            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) *
-            sin((lat * pi()/180.0 - :lat * pi()/180.0)/2) +
-            cos(:lat * pi()/180.0) * cos(lat * pi()/180.0) *
-            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2) *
-            sin((lon * pi()/180.0 - :lon * pi()/180.0)/2)
-        )
-    ), 0) AS distance
+    *
 FROM
-    gas_stations
+    (SELECT
+        gas_stations.id AS id
+        , gas_stations.name AS name
+        , gas_stations.lat AS lat
+        , gas_stations.lon AS lon
+        , ROUND(2.0 * 6371.0 * 1000 * 
+            ASIN(
+                SQRT(
+                    SIN((gas_stations.lat * pi()/180.0 - :lat * pi()/180.0)/2) *
+                    SIN((gas_stations.lat * pi()/180.0 - :lat * pi()/180.0)/2) +
+                    COS(:lat * pi()/180.0) * COS(gas_stations.lat * pi()/180.0) *
+                    SIN((gas_stations.lon * pi()/180.0 - :lon * pi()/180.0)/2) *
+                    SIN((gas_stations.lon * pi()/180.0 - :lon * pi()/180.0)/2)
+                )), 0) AS distance
+    FROM
+        gas_stations)
+WHERE
+    distance < :radius
+ORDER BY
+    distance
 """
