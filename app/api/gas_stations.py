bad0e1918fa4c3aa8556544a0486fa83ed72dae0
MP <paczesnymaciej@gmail.com>
2022-12-22 23:56:22 +0100

    refactor; fix pagination when lat, lon, radius provided; add name, fuel params

--- app/api/gas_stations.py
+++ app/api/gas_stations.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request, url_for, current_app
-from sqlalchemy.sql import text
+from sqlalchemy.sql import text, func, select, column
 
-from ..models import GasStation
+from ..models import GasStation, Fuel
 from ..sql import sql
 from . import api
 
@@ -12,32 +12,46 @@ def get_gas_stations():
     lat = request.args.get('lat', type=float)
     lon = request.args.get('lon', type=float)
     radius = request.args.get('radius', type=int)
+    name = request.args.get('name', type=str)
+    if name:
+        name = f"%{name.lower()}%"
+    fuel = request.args.get('fuel', type=str)
+    if fuel:
+        fuel = f"%{fuel.lower()}%"
     per_page = request.args.get('per_page', type=int)
     if not per_page:
         per_page = current_app.config['STATIONS_PER_PAGE']
-    prev, next = None, None
+    next, prev = None, None
+
+    query = GasStation.query
+    if name:
+        query = query.filter(func.lower(GasStation.name).like(name))
+
+    if fuel:
+        query = query.filter(func.lower(Fuel.name).like(fuel))
 
     if lat and lon and radius:
-        text_sql = text(sql.select_gas_stations_with_distance)
-        text_sql = text_sql.columns(GasStation.id, GasStation.name, GasStation.lat,
-            GasStation.lon, GasStation.distance)
-        text_sql = text_sql.select().limit(per_page).offset(per_page * (page-1))
-        query = GasStation.query.from_statement(text_sql)
-        stations = query.params(lat=lat, lon=lon, radius=radius).all()
-        total = len(stations)
+        text_sql = sql.select_gas_stations_with_distance.replace(":lon", str(lon)).\
+            replace(":lat", str(lat))
+        text_sql = text(text_sql)
+        cte = select([column('id'), column('harvesine')], use_labels=True).select_from(text_sql).\
+            cte("cte")
+        query = query.join(cte, cte.columns["id"] == GasStation.id)
+        query = query.filter(column('harvesine') < radius)
 
-    else:
-        query = GasStation.query
-        pagination = query.order_by(GasStation.id).paginate(
-                page=page, per_page=per_page,
-                error_out=False)
-        stations = pagination.items
+    pagination = query.order_by(GasStation.id).paginate(
+            page=page,
+            per_page=per_page,
+            error_out=False
+            )
+    stations = pagination.items
 
-        if pagination.has_prev:
-            prev = url_for('api.get_gas_stations', page=page-1)
-        if pagination.has_next:
-            next = url_for('api.get_gas_stations', page=page+1)
-        total = pagination.total
+    if pagination.has_prev:
+        prev = url_for('api.get_gas_stations', page=page-1)
+    if pagination.has_next:
+        next = url_for('api.get_gas_stations', page=page+1)
+
+    total = pagination.total
 
     return jsonify({
         'gas_stations': [station.to_json() for station in stations],
