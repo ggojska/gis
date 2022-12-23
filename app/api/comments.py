c0235628e6fa85d8878c1f82934cb88baa1ce097
MP <paczesnymaciej@gmail.com>
2022-12-23 20:25:41 +0100

    add comments api

---
+++ app/api/comments.py
@@ -0,0 +1,52 @@
+from flask import jsonify, request, url_for, current_app
+
+from ..models import GasStation, Comment
+from ..sql import sql
+from . import api
+
+
+@api.route('/comments/')
+def get_comments():
+    page = request.args.get('page', 1, type=int)
+    pagination = Comment.query.order_by(Comment.created_at.desc()).paginate(
+        page=page, per_page=current_app.config['COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    prev, next = None, None
+
+    if pagination.has_prev:
+        prev = url_for('api.get_comments', page=page-1)
+
+    if pagination.has_next:
+        next = url_for('api.get_comments', page=page+1)
+
+    return jsonify({
+        'comments': [comment.to_json() for comment in comments],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
+
+
+@api.route('/gas_stations/<int:id>/comments')
+def get_gas_station_comments(id):
+    station = GasStation.query.get_or_404(id)
+    page = request.args.get('page', 1, type=int)
+    pagination = station.comments.order_by(Comment.created_at.asc()).paginate(
+        page=page, per_page=current_app.config['COMMENTS_PER_PAGE'],
+        error_out=False)
+    comments = pagination.items
+    prev, next = None, None
+
+    if pagination.has_prev:
+        prev = url_for('api.get_post_comments', id=id, page=page-1)
+
+    if pagination.has_next:
+        next = url_for('api.get_post_comments', id=id, page=page+1)
+
+    return jsonify({
+        'comments': [comment.to_json() for comment in comments],
+        'prev': prev,
+        'next': next,
+        'count': pagination.total
+    })
