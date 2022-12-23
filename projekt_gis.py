9b5ab9b3bc1a4b6fad1cb770d8f51912086bc342
MP <paczesnymaciej@gmail.com>
2022-12-24 00:01:43 +0100

    add cli command to import sample data

--- projekt_gis.py
+++ projekt_gis.py
@@ -7,14 +7,18 @@ from app import create_app, db
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
-with app.app_context():
-    if os.getenv('IMPORT_SAMPLE_DATA') == "1":
+@app.cli.command()
+def import_sample_data():
+    with app.app_context():
         basedir = os.path.abspath(os.path.dirname(__file__))
         path = os.path.join(basedir, "sample_data", "sample_data.sql")
         if os.path.exists(path):
             print("Importing sample data...")
             with open(path, "r", encoding="utf-8") as file:
-                data = file.readlines()
-                for line in data:
+                data = file.read()
+                for line in data.split(";"):
                     db.session.execute(line)
-                db.session.commit()
+                    try:
+                        db.session.commit()
+                    except:
+                        db.session.rollback()
