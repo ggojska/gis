09ecd885836849b06eab900c6aece1be784208ab
MP <paczesnymaciej@gmail.com>
2022-12-24 09:54:21 +0100

    add proper cli command to import data

--- projekt_gis.py
+++ projekt_gis.py
@@ -7,18 +7,18 @@ from app import create_app, db
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
-@app.cli.command()
+@app.cli.command("import-sample")
 def import_sample_data():
-    with app.app_context():
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        path = os.path.join(basedir, "sample_data", "sample_data.sql")
-        if os.path.exists(path):
-            print("Importing sample data...")
-            with open(path, "r", encoding="utf-8") as file:
-                data = file.read()
-                for line in data.split(";"):
-                    db.session.execute(line)
-                    try:
-                        db.session.commit()
-                    except:
-                        db.session.rollback()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(basedir, "sample_data", "sample_data.sql")
+    if os.path.exists(path):
+        print("Importing sample data...")
+        with open(path, "r", encoding="utf-8") as file:
+            data = file.read().split(";")
+            count = len(data)
+            ten_percent = int(count/10)
+            for i in range(0, count):
+                db.session.execute(data[i])
+                if i % ten_percent == 0:
+                    print(f"Importing sample data {round(100 * i/count, 0)}")
+            db.session.commit()
