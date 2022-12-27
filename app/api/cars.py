f3f50f8188f87cf6b55c60380313b2b9bcc8b2c1
MP <paczesnymaciej@gmail.com>
2022-12-27 22:08:28 +0100

    add cars api authorization

--- app/api/cars.py
+++ app/api/cars.py
@@ -8,17 +8,30 @@ from . import api, errors
 
 @api.route('/cars/<int:id>')
 def get_car(id):
+    if not g.get("current_user"):
+        return errors.unauthorized("operacja dozwolona tylko dla zalogowanego użytkownika")
+
     car = Car.query.get(id)
     if not car:
          return errors.not_found(f'nie znaleziono samochodu')
+
+    if g.current_user != car.user:
+        return errors.forbidden('niedozwolone')
+
     return jsonify(car.to_json())
 
 
 @api.route('/users/<int:id>/cars')
 def get_user_cars(id):
+    if not g.get("current_user"):
+        return errors.unauthorized("operacja dozwolona tylko dla zalogowanego użytkownika")
+
     user = User.query.get(id)
     if not user:
          return errors.not_found(f'użytkownik o id {id} nie istnieje')
+
+    if g.current_user != user:
+        return errors.forbidden('niedozwolone')
 
     return jsonify({
         'cars': [car.to_json() for car in user.cars],
@@ -33,7 +46,10 @@ def add_new_car(id):
     user = User.query.get(id)
     if not user:
          return errors.not_found(f'użytkownik o id {id} nie istnieje')
-    
+
+    if g.current_user != user:
+        return errors.forbidden('nie można dodawać samochodów do użytkowników')
+
     car = Car.from_json(request.json)
     car.user_id = id
     db.session.add(car)
