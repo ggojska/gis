1d187fe854f5c4ee4a99223adc78b38982b3dd97
MP <paczesnymaciej@gmail.com>
2022-12-29 11:47:15 +0100

    add CarForm

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,17 @@
-# TODO
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DecimalField
+from wtforms.validators import DataRequired, Length
+
+from ..models import Car
+
+
+class CarForm(FlaskForm):
+    make = StringField('Marka', validators=[DataRequired(),
+        Length(1, 20, message="Marka samochodu musi być uzupełniona."),])
+    model = StringField('Model', validators=[DataRequired(),
+        Length(1, 20, message="Model samochodu musi być uzupełniona."),])
+    combustion = DecimalField('Spalanie', places=1, validators=[DataRequired(),])
+    fuel = StringField('Paliwo', validators=[DataRequired(),
+        Length(1, 10, message="Paliwo samochodu musi być uzupełnione."),])
+    submit = SubmitField('Dodaj')
+    submit.label = None
--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,25 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import login_required, current_user
+from flask import render_template, flash
+from flask_login import login_required
 
 from . import main
 from .. import db
 from ..models import Car
+from .forms import CarForm
 
 
 @main.route('/', methods=['GET'])
 def index():
     return render_template('index.html', mapa=True)
 
-@main.route('/my-account', methods=['GET'])
+@main.route('/my-account', methods=['GET', 'POST'])
 @login_required
 def my_account():
-    return render_template('my_account.html')
+    form = CarForm()
+    if form.validate_on_submit():
+        car = Car(make=form.make.data,
+                    model=form.model.data,
+                    combustion=form.combustion.data,
+                    fuel=form.fuel.data)
+        db.session.add(car)
+        db.session.commit()
+    return render_template('my_account.html', form=form)
