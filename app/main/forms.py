99e8bc97aa6dd92c92ee090dff9883ca25fc56f8
MP <paczesnymaciej@gmail.com>
2022-12-29 12:35:40 +0100

    add combustion step; change fuel label text

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,8 +7,9 @@ class CarForm(FlaskForm):
     make = StringField('Marka', validators=[DataRequired(),
         Length(1, 20, message="Marka samochodu musi być uzupełniona."),])
     model = StringField('Model', validators=[DataRequired(),
-        Length(1, 20, message="Model samochodu musi być uzupełniona."),])
-    combustion = DecimalField('Spalanie', places=1, validators=[DataRequired(),])
-    fuel = SelectField(u'Paliwo', validators=[DataRequired(),])
+        Length(1, 20, message="Model samochodu musi być uzupełniony."),])
+    combustion = DecimalField('Spalanie', places=1, validators=[DataRequired(),],
+        render_kw={"step": "0.1"})
+    fuel = SelectField(u'Rodzaj paliwa', validators=[DataRequired(),])
     submit = SubmitField('Dodaj')
     submit.label = None
