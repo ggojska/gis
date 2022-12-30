70819f3387194463b20c6f73fb9a79ad6a8d7c2f
MP <paczesnymaciej@gmail.com>
2022-12-30 11:04:50 +0100

    add HTML endpoints

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,9 @@ from flask_login import login_required, current_user
 
 from . import main
 from .. import db
-from ..models import Car, fuels_lookup, db
+from ..models import GasStation, Car, fuels_lookup, db
 from .forms import CarForm
+from .errors import page_not_found
 
 
 @main.route('/', methods=['GET'])
@@ -26,3 +27,17 @@ def my_account():
         db.session.add(car)
         db.session.commit()
     return render_template('my_account.html', form=form)
+
+@main.route('/gas_stations/<int:id>/popup', methods=['GET'])
+def gas_station_popup(id):
+    station = GasStation.query.get(id)
+    if not station:
+         return page_not_found()
+    return render_template('_gas_station_popup.html', station=station)
+
+@main.route('/gas_stations/<int:id>/bigpopup', methods=['GET'])
+def gas_station_big_popup(id):
+    station = GasStation.query.get(id)
+    if not station:
+         return page_not_found()
+    return render_template('_gas_station_big_popup.html', station=station)
