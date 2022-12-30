4d275ceea82629460b860de571168393194bd4e2
MP <paczesnymaciej@gmail.com>
2022-12-30 17:09:16 +0100

    add html endpoint for deleting car

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from flask_login import login_required, current_user
 
 from . import main
@@ -27,6 +27,21 @@ def my_account():
         db.session.add(car)
         db.session.commit()
     return render_template('my_account.html', form=form)
+
+@main.route('/cars/<int:id>/delete', methods=['POST'])
+@login_required
+def delete_car(id):
+    car = Car.query.get(id)
+    if not car:
+        flash('brak takiego samochodu')
+        return redirect(url_for('main.my_account'))
+    if current_user != car.user:
+        flash('nie można usunąc nieswojego samochodu')
+        return redirect(url_for('main.my_account'))
+    db.session.delete(car)
+    db.session.commit()
+    flash('usunięto samochód')
+    return redirect(url_for('main.my_account'))
 
 @main.route('/gas_stations/<int:id>/popup', methods=['GET'])
 def gas_station_popup(id):
