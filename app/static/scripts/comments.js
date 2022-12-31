6f840d7a1cd58afc10a3995aa6f1fa73de844e0d
MP <paczesnymaciej@gmail.com>
2022-12-31 20:47:25 +0100

    add

---
+++ app/static/scripts/comments.js
@@ -0,0 +1,9 @@
+const api_url = "/api/v1";
+
+function editComment(commentId) {
+    console.log("Edit comment " + commentId)
+}
+
+function deleteComment(commentId) {
+    console.log("Delete comment " + commentId)
+}
